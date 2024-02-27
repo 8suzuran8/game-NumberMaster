22152382ea3ee33af76a2001f081d17d4019ea76
8suzuran8 <8suzuran8@gmail.com>
2024-02-27 13:46:52 +0900

    Correspond to various updates of AndroidStudio. Swipe processing now supports API level 34.

--- app/src/main/java/com/example/numbermaster/NumberMasterOnSwipeTouchListener.kt
+++ app/src/main/java/com/example/numbermaster/NumberMasterOnSwipeTouchListener.kt
@@ -4,12 +4,19 @@ import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import kotlin.math.abs
 
 open class NumberMasterOnSwipeTouchListener(context: AppCompatActivity): View.OnTouchListener {
-
     private val gestureDetector: GestureDetector
     var flingResult: Boolean? = null
+
+    private var e1: MutableMap<String, Float?> = mutableMapOf(
+        "x" to null,
+        "y" to null,
+    )
+    private var e2: MutableMap<String, Float?> = mutableMapOf(
+        "x" to null,
+        "y" to null,
+    )
 
     companion object {
         private const val SWIPE_THRESHOLD = 100
@@ -23,43 +30,64 @@ open class NumberMasterOnSwipeTouchListener(context: AppCompatActivity): View.On
     override fun onTouch(v: View, event: MotionEvent): Boolean {
         v.performClick()
 
-        this.gestureDetector.setIsLongpressEnabled(false)
-        return this.gestureDetector.onTouchEvent(event)
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                if (e1["x"] == null) {
+                    this.e1["x"] = event.x
+                    this.e1["y"] = event.y
+                }
+            }
+            MotionEvent.ACTION_UP -> {
+                this.e2["x"] = event.x
+                this.e2["y"] = event.y
+
+                var result = false
+                try {
+                    val diffY = this.e2["y"]!! - this.e1["y"]!!
+                    val diffX = this.e2["x"]!! - this.e1["x"]!!
+
+                    if (kotlin.math.abs(diffX) > kotlin.math.abs(diffY) && kotlin.math.abs(diffX) > SWIPE_THRESHOLD) {
+                        if (diffX > 0) {
+                            onSwipeRight(0, e1["x"], e1["y"])
+                        } else {
+                            onSwipeLeft(0, e1["x"], e1["y"])
+                        }
+                        result = true
+                    } else if (kotlin.math.abs(diffX) < kotlin.math.abs(diffY) && kotlin.math.abs(diffY) > SWIPE_VELOCITY_THRESHOLD) {
+                        if (diffY > 0) {
+                            onSwipeBottom(0, e1["x"], e1["y"])
+                        } else {
+                            onSwipeTop(0, e1["x"], e1["y"])
+                        }
+                        result = true
+                    } else {
+                        this.e1["x"] = null
+                        this.e1["y"] = null
+
+                        this.gestureDetector.setIsLongpressEnabled(false)
+                        return this.gestureDetector.onTouchEvent(event)
+                    }
+                } catch (exception: Exception) {
+                    this.e1["x"] = null
+                    this.e1["y"] = null
+
+                    exception.printStackTrace()
+                }
+
+                this.e1["x"] = null
+                this.e1["y"] = null
+
+                flingResult = result
+                return result
+            }
+        }
+
+        return true
     }
 
     private inner class GestureListener : GestureDetector.SimpleOnGestureListener() {
         override fun onDown(e: MotionEvent): Boolean {
             return true
-        }
-
-        override fun onFling(e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
-            var result = false
-            try {
-                val diffY = e2.y - e1.y
-                val diffX = e2.x - e1.x
-                if (abs(diffX) > abs(diffY)) {
-                    if (abs(diffX) > SWIPE_THRESHOLD && abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
-                        if (diffX > 0) {
-                            onSwipeRight(0, e1.x, e1.y)
-                        } else {
-                            onSwipeLeft(0, e1.x, e1.y)
-                        }
-                        result = true
-                    }
-                } else if (abs(diffY) > SWIPE_THRESHOLD && abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
-                    if (diffY > 0) {
-                        onSwipeBottom(0, e1.x, e1.y)
-                    } else {
-                        onSwipeTop(0, e1.x, e1.y)
-                    }
-                    result = true
-                }
-            } catch (exception: Exception) {
-                exception.printStackTrace()
-            }
-
-            flingResult = result
-            return result
         }
     }
 
